962223b957a7730878f5444ad1adecf91a9914e7
DoormatIka <trashcanartistlily@gmail.com>
2023-08-09 01:17:12 +0900

    dangerous command

---
+++ commands/stop.ts
@@ -0,0 +1,10 @@
+import { Client, Message } from "discord.js";
+
+
+export const name = "stop";
+export const cooldown = 5;
+export async function execute(client: Client, msg: Message) {
+    if (msg.author.id === "509683395224141827") {
+        process.exit();
+    }
+}
