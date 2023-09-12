3c24379924d36ad7ef75ad765d4160e1bdc22c4d
DoormatIka <trashcanartistlily@gmail.com>
2023-09-13 01:03:47 +0800

    mentions support added

--- src/commands/quote.ts
+++ src/commands/quote.ts
@@ -15,12 +15,15 @@ export async function execute(client: Client, msg: Message) {
     
     const mimetype = mime.lookup(replied.attachments.at(0) 
         ? replied.attachments.at(0)!.url
-        : "")
+        : "");
+
+    const parsed_content = await parseQuotes(client, replied.content);
+
     try {
         msg.reply({
             files: [{
                 attachment: await quote(
-                    replied.content, 
+                    parsed_content, 
                     replied.author.displayName,
                     replied.author.displayAvatarURL({ size: 1024 }),
                     mimetype,
@@ -55,4 +58,44 @@ async function quote(
         }
     }
     return quoteDefault(text, author, avatar_url);
+}
+
+async function parseQuotes(client: Client, str: string) {
+    const parsed_mentions = await extractObjects(
+        str, 
+        /(?<=<@)\d+(?=>)/g, 
+        /<@\d+>/g,
+        async (extracted) => {
+            const user = client.users.cache.get(extracted) ?? await client.users.fetch(extracted)
+            return user.displayName 
+                ? user.displayName 
+                : user.username;
+        }
+    );
+    
+    const parsed_emotes = await extractObjects(
+        parsed_mentions,
+        /(?<=<:(.+?))\d+(?=>)/g, 
+        /<:(.+?):\d+>/g,
+        async (extracted) => {
+            // placeholder for the replacement algorithm for emojis
+            return "";
+        }
+    )
+    
+    return parsed_emotes;
+}
+
+async function extractObjects(
+    str: string, 
+    extract: RegExp, 
+    replace: RegExp,
+    replacer: (extracted: string) => Promise<string>,
+) {
+    let string = str.slice();
+    const extracted = str.match(extract);
+    for (const extract of extracted ?? []) {
+        string = string.replace(replace, await replacer(extract));
+    }
+    return string;
 }
