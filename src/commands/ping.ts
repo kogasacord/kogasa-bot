fbaca0f3b614a0fa1a53c416a249e0686bfd9150
DoormatIka <trashcanartistlily@gmail.com>
2023-09-25 18:07:06 +0800

    added args into ping

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -3,6 +3,6 @@ import { Client, Message } from "discord.js";
 export const name = "ping";
 export const cooldown = 5;
 export const description = "Test command."
-export async function execute(client: Client, msg: Message) {
+export async function execute(client: Client, msg: Message, args: string[]) {
     msg.reply(`Pong.`);
 }
