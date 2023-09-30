8f759b82c3993c7f3ea1569721c05045b21582e9
DoormatIka <trashcanartistlily@gmail.com>
2023-09-30 20:13:50 +0800

    chat & README.md

--- README.md
+++ README.md
@@ -9,13 +9,15 @@ Prefix: `??`
 - `ytdl [link] [format_id]`: Download videos from YouTube. Requires [ytdlp-drive](https://github.com/DoormatIka/ytdlp-drive-https-api).
 - `ytdlf`: Get the quality and format of your video. Can be used with `ytdl` with a `format_id`.
 - `google [search_query]` and `img [search_query]`: Gets google searches and images.
+- `randomweb`: Sends you a random website with gacha elements snuck in.
+- `sauce`: Reply to an image to find the source of the image relating to anime.
+- `help`: Finally, a help command!
+- `set [command_name/"all"] [channel_id/"all"] [true/false]`: Settings.
+- `doctor`: Check if your servers are working.
 
 **Other features**:
 
 A basic file structure for easy additions to the bot.
-
-
-
 
 Why is there SO MUCH HTTP servers??
 - It seemed cool to implement but it got out of hand quickly. It also has the perk of getting faster results than only one process handling everything.
---
+++ src/commands/chat.ts
@@ -0,0 +1,33 @@
+import fetch from "node-fetch";
+import { Client, Message } from "discord.js";
+
+export const name = "chat";
+export const cooldown = 40;
+export const description = "Chat with Kogasa! (Model: Base Llama2 7B)"
+export async function execute(client: Client, msg: Message, args: string[]) {
+    const user_message = args.join(" ");
+    if (user_message.length <= 0) {
+        msg.reply("Send me a message!")
+        return;
+    }
+    if (user_message.length > 200) {
+        msg.reply("Your message is too long.")
+        return;
+    }
+    await msg.channel.sendTyping()
+    const llama_response = await messageLlama2B(user_message)
+    msg.reply(llama_response.response);
+}
+
+
+export async function messageLlama2B(msg: string) {
+    const llama = await fetch("http://localhost:5000/", {
+        method: "POST",
+        body: JSON.stringify({ msg: msg }),
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+    });
+    return await llama.json() as { response: string };
+}
