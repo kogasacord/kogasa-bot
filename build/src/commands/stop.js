0da59397ea7e5cb0b75a5cf7135f8216a741f5c2
DoormatIka <trashcanartistlily@gmail.com>
2023-10-24 17:19:29 +0800

    actually removing the build folder

--- build/src/commands/stop.js
+++
@@ -1,9 +0,0 @@
-export const name = "stop";
-export const cooldown = 5;
-export const description = "You can only use this when you are my creator.";
-export async function execute(client, msg) {
-    if (msg.author.id === "509683395224141827") {
-        msg.reply("My work here is done.");
-        process.exit();
-    }
-}
--- build/src/helpers/ytdl/get_status.js
+++
@@ -1,7 +0,0 @@
-import fetch from "node-fetch";
-export async function getStatus() {
-    const status = await fetch("http://localhost:3000/status", {
-        method: "GET",
-    });
-    return await status.json();
-}
