35a3d8f4a8823242fdc2e07a44bb04ef47d2dbd7
DoormatIka <trashcanartistlily@gmail.com>
2023-10-27 11:34:23 +0800

    fix: ping with Linux and Windows

--- src/helpers/misc/ping.ts
+++ src/helpers/misc/ping.ts
@@ -1,15 +1,19 @@
 import child_process from "child_process";
+import os from "os";
 
 interface Ping {
 	bytes: number,
 	time: number,
-	TTL: number,
+	TTL?: number,
+	ttl?: number,
 }
-
+/*
+* This function requires admin privileges for Windows.
+*/
 export function pingURL(url: string) {
 	return new Promise<Ping[]>((res, rej) => {
 		const pings: Ping[] = [];
-		const proc = child_process.spawn("ping", [url])
+		const proc = child_process.spawn("ping", [optionforOS(), "2", url])
 		proc.stdout.on("data", (chunk: Buffer) => {
 			const regex = [...chunk.toString().matchAll(/\w+=\d+/g)];
 			if (regex.length < 3) {
@@ -20,7 +24,7 @@ export function pingURL(url: string) {
 			for (const results of regex) {
 				const split = results[0].split("=");
 				if (split.length > 0)
-					ping.set(split[0], parseInt(split[1]))
+					ping.set(split[0], parseInt(split[1]));
 			}
 			pings.push(Object.fromEntries(ping) as any);
 		})
@@ -33,3 +37,13 @@ export function pingURL(url: string) {
 	})
 }
 
+function optionforOS() {
+	const OS: "Windows_NT" | "Linux" | "Darwin" = os.type() as any;
+	if (OS === "Windows_NT")
+		return "-n"
+	if (OS === "Linux")
+		return "-c"
+	throw Error("Unimplemented OS for ping.")
+}
+
+console.log(await pingURL("discord.com"));
