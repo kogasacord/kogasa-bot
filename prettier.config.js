3bdf9f242ae378e2ee7cf205bb2e5d79d8a750e5
DoormatIka <trashcanartistlily@gmail.com>
2023-12-17 23:37:36 +0800

    added eslint

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,12 @@
 /** @type {import("prettier").Config} */
 const config = {
-  trailingComma: "es5",
-  tabWidth: 2,
-  semi: true,
-  singleQuote: false,
-  bracketSpacing: true,
-  arrowParens: "always",
+	trailingComma: "es5",
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	singleQuote: false,
+	bracketSpacing: true,
+	arrowParens: "always",
 };
 
 export default config;
