74a63950fe556b8a6673edaba50bd121cfa11cd6
DoormatIka <trashcanartistlily@gmail.com>
2024-06-28 23:15:58 +0800

    db setup done

--- src/commands/confess.ts
+++ src/commands/confess.ts
@@ -1,7 +1,7 @@
 import {ExternalDependencies} from "@helpers/types";
 import { ChannelType, Client, Message } from "discord.js";
 import { ChannelScope } from "@helpers/types";
-import Pocketbase, { RecordModel } from "pocketbase";
+import Pocketbase, { RecordModel, RecordService } from "pocketbase";
 import { createHash } from "node:crypto";
 
 interface PBGuild extends RecordModel {
@@ -13,12 +13,12 @@ interface PBChannels extends RecordModel {
 
 // i dont know what's happening to this code anymore.
 async function setup(msg: Message, pb: Pocketbase) {
-	const msg_id = createHash("sha256")
+	const channel_hash = createHash("sha256")
 		.update(msg.channel.id)
 		.digest("hex")
 		.toString()
 		.slice(0, 15);
-	const guild_id = createHash("sha256")
+	const guild_hash = createHash("sha256")
 		.update(msg.guild!.id)
 		.digest("hex")
 		.toString()
@@ -26,19 +26,29 @@ async function setup(msg: Message, pb: Pocketbase) {
 	const pb_channels = pb.collection("channels");
 	const pb_guild = pb.collection("guild");
 
-	let confess_channel;
-	try {
-		confess_channel = await pb_channels.getOne(msg_id);
-	} catch (error) {
-		confess_channel = await pb_channels.create<PBChannels>({ id: msg_id, confess: true });
+	// makes a channel, makes a guild (if unavailable), connects those two.
+	let confess_channel = await getRecord<PBChannels>(pb_channels, channel_hash);
+	if (confess_channel) {
+		await pb_channels.delete(confess_channel.id);
 	}
-	try { // wow this sucks.
-		// so im relying on getOne to throw an error if there's no record found, this is the result.
-		const guild = await pb_guild.getOne<PBGuild>(guild_id);
-		await pb_channels.delete(guild.confess);
-		await pb_guild.update(guild.id, { confess: confess_channel.id });
-	} catch (error) { // i hate trycatch.
-		await pb_guild.create({ id: msg.guild!.id, confess: confess_channel.id });
+	confess_channel = await pb_channels.create<PBChannels>({ id: channel_hash, confess: true });
+	const guild = await getRecord<PBGuild>(pb_guild, guild_hash);
+	if (guild) {
+		const previous_channel = await getRecord<PBChannels>(pb_channels, guild.confess);
+		if (previous_channel) {
+			pb_channels.delete(previous_channel.id);
+		}
+		await pb_guild.update(guild.id, { "confess": [confess_channel.id] });
+	} else {
+		await pb_guild.create({ "id": guild_hash, "confess": [confess_channel.id] });
+	}
+}
+
+async function getRecord<T extends RecordModel>(collection: RecordService<T>, id: string): Promise<T | undefined> {
+	try {
+		return await collection.getOne(id);
+	} catch (error) {
+		return undefined;
 	}
 }
 
